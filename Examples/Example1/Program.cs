52a790f5e54ab3684a337bd07295ab47dcc657cd
Chris Anderson <chrisa23@gmail.com>
2020-01-12 14:38:37 -0600

    Update Program.cs

--- Examples/Example1/Program.cs
+++ Examples/Example1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Fibrous;
 using Fibrous.Agents;
 
@@ -7,6 +8,27 @@ namespace Example1
     internal class Program
     {
         private static void Main()
+        {
+            SimpleExample();
+            MoreComplexExample();
+            AgentExample();
+        }
+
+        private static void MoreComplexExample()
+        {
+            using var calculator = new Calculator();
+            calculator.Publish(new Message(3, Operation.Add));
+            calculator.Publish(new Message(24, Operation.Add));
+            calculator.Publish(new Message(3, Operation.Divide));
+            calculator.Publish(new Message(1.5, Operation.Multiply));
+
+            var result = calculator.SendRequest(null).Result;
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        private static void SimpleExample()
         {
             using var fiber = new Fiber();
             var channel = fiber.NewChannel<string>(Console.WriteLine);
@@ -28,6 +50,97 @@ namespace Example1
             var count = 0;
             //Start sending a message after a second, every 2 seconds...
             fiber1.Schedule(() => processor.Publish("Test" + count++), TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2));
+            Console.ReadKey();
         }
+    }
+
+    public class Calculator : IPublisherPort<Message>, IRequestPort<object,double>, IDisposable
+    {
+        readonly IFiber _fiber;
+        readonly IChannel<Message> _channel;
+        readonly IRequestPort<object, double> _request;
+        double _current;
+        public Calculator()
+        {
+            _fiber = new Fiber(OnError);
+            _channel = _fiber.NewChannel<Message>(OnMessage);
+            _request = _fiber.NewRequestPort<object, double>(OnRequest);
+        }
+
+        private void OnRequest(IRequest<object, double> obj)
+        {
+            obj.Reply(_current);
+        }
+
+        private void OnMessage(Message obj)
+        {
+            switch (obj.Operation)
+            {
+                case Operation.Add:
+                    _current += obj.Value;
+                    break;
+                case Operation.Subtract:
+                    _current -= obj.Value;
+                    break;
+                case Operation.Multiply:
+                    _current /= obj.Value;
+                    break;
+                case Operation.Divide:
+                    _current *= obj.Value;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void OnError(Exception obj)
+        {
+            Console.WriteLine(obj);
+        }
+
+        public void Publish(Message msg)
+        {
+            _channel.Publish(msg);
+        }
+
+        public IDisposable SendRequest(object request, IFiber fiber, Action<double> onReply)
+        {
+            return _request.SendRequest(request, fiber, onReply);
+        }
+
+        public IDisposable SendRequest(object request, IAsyncFiber fiber, Func<double, Task> onReply)
+        {
+            return _request.SendRequest(request, fiber, onReply);
+        }
+
+        public Task<double> SendRequest(object request)
+        {
+            return _request.SendRequest(request);
+        }
+
+        public void Dispose()
+        {
+            _fiber?.Dispose();
+        }
+    }
+
+    public class Message
+    {
+        public double Value { get; set; }
+        public Operation Operation { get; set; }
+
+        public Message(double value, Operation operation)
+        {
+            Value = value;
+            Operation = operation;
+        }
+    }
+
+    public enum Operation
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide
     }
 }
