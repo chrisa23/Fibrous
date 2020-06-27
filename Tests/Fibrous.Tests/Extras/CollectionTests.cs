561e0cb3c334448c994434327a3d164c31bf9b4a
Chris Anderson <chrisa23@gmail.com>
2020-06-27 08:07:24 -0500

    Removed Thread Sleep from tests

--- Tests/Fibrous.Tests/Extras/CollectionTests.cs
+++ Tests/Fibrous.Tests/Extras/CollectionTests.cs
@@ -14,6 +14,7 @@ namespace Fibrous.Tests
             int[] snapshot = null;
             var list = new List<int>();
             using var collection = new FiberCollection<int>();
+            using var reset = new AutoResetEvent(false);
             using var receive = new Fiber();
             collection.Add(1);
             collection.Add(2);
@@ -24,19 +25,31 @@ namespace Fibrous.Tests
                         list.Add(action.Items[0]);
                     else
                         list.Remove(action.Items[0]);
+                    reset.Set();
                 },
-                ints => snapshot = ints);
-            Thread.Sleep(10);
+                ints =>
+                {
+                    snapshot = ints;
+                    reset.Set();
+                });
+            
+            Assert.IsTrue(reset.WaitOne(1000));
+            
             Assert.AreEqual(2, snapshot.Length);
             Assert.AreEqual(1, snapshot[0]);
             Assert.AreEqual(2, snapshot[1]);
             Assert.AreEqual(0, list.Count);
+            
             collection.Add(3);
-            Thread.Sleep(10);
+            Assert.IsTrue(reset.WaitOne(1000));
+            
             Assert.AreEqual(1, list.Count);
+            
             collection.Remove(3);
-            Thread.Sleep(10);
+            Assert.IsTrue(reset.WaitOne(1000));
+            
             Assert.AreEqual(0, list.Count);
+            
             var items = collection.GetItems(x => true);
             Assert.AreEqual(2, items.Length);
         }
@@ -47,6 +60,7 @@ namespace Fibrous.Tests
             int[] snapshot = null;
             var list = new List<int>();
             using var collection = new FiberKeyedCollection<int, int>(x => x);
+            using var reset = new AutoResetEvent(false);
             using var receive = new Fiber();
             collection.Add(1);
             collection.Add(2);
@@ -57,19 +71,31 @@ namespace Fibrous.Tests
                         list.Add(action.Items[0]);
                     else
                         list.Remove(action.Items[0]);
+                    reset.Set();
                 },
-                ints => snapshot = ints);
-            Thread.Sleep(10);
+                ints =>
+                {
+                    snapshot = ints;
+                    reset.Set();
+                });
+            
+            Assert.IsTrue(reset.WaitOne(1000));
+            
             Assert.AreEqual(2, snapshot.Length);
             Assert.AreEqual(1, snapshot[0]);
             Assert.AreEqual(2, snapshot[1]);
             Assert.AreEqual(0, list.Count);
+            
             collection.Add(3);
-            Thread.Sleep(10);
+            Assert.IsTrue(reset.WaitOne(1000));
+
             Assert.AreEqual(1, list.Count);
+            
             collection.Remove(3);
-            Thread.Sleep(10);
+            Assert.IsTrue(reset.WaitOne(1000));
+            
             Assert.AreEqual(0, list.Count);
+            
             var items = collection.GetItems(x => true);
             Assert.AreEqual(2, items.Length);
         }
@@ -80,6 +106,7 @@ namespace Fibrous.Tests
             KeyValuePair<int, int>[] snapshot = null;
             var list = new List<KeyValuePair<int, int>>();
             using var collection = new FiberDictionary<int, int>();
+            using var reset = new AutoResetEvent(false);
             using var receive = new Fiber();
             collection.Add(1, 1);
             collection.Add(2, 2);
@@ -90,22 +117,36 @@ namespace Fibrous.Tests
                         list.Add(action.Items[0]);
                     else
                         list.Remove(action.Items[0]);
+
+                    reset.Set();
                 },
-                ints => snapshot = ints);
-            Thread.Sleep(10);
+                ints =>
+                {
+                    snapshot = ints;
+                    reset.Set();
+                });
+
+            Assert.IsTrue(reset.WaitOne(1000));
+
             Assert.AreEqual(2, snapshot.Length);
             Assert.AreEqual(1, snapshot[0].Key);
             Assert.AreEqual(2, snapshot[1].Key);
             Assert.AreEqual(0, list.Count);
+            
             collection.Add(3, 3);
-            Thread.Sleep(10);
+            Assert.IsTrue(reset.WaitOne(1000));
+            
             Assert.AreEqual(1, list.Count);
+            
             collection.Remove(3);
-            Thread.Sleep(10);
+            Assert.IsTrue(reset.WaitOne(1000));
+            
             Assert.AreEqual(0, list.Count);
+            
             var items = collection.GetItems(x => true);
             Assert.AreEqual(2, items.Length);
             collection.Clear();
+            
             items = collection.GetItems(x => true);
             Assert.AreEqual(0, items.Length);
         }
@@ -118,23 +159,27 @@ namespace Fibrous.Tests
             collection.Add(2, 2);
 
             var local = new Dictionary<int, int>();
+
+            using var reset = new AutoResetEvent(false);
             using var fiber = new Fiber();
             
             //Snapshot after subscribe local copy
-            collection.SubscribeLocalCopy(fiber, local, () =>{ });
-            Thread.Sleep(10);
+            collection.SubscribeLocalCopy(fiber, local, () =>reset.Set());
+
+            Assert.IsTrue(reset.WaitOne(1000));
+
             Assert.AreEqual(2, local.Count);
             Assert.AreEqual(1, local[1]);
             Assert.AreEqual(2, local[2]);
 
             //Add
             collection.Add(3, 3);
-            Thread.Sleep(10);
+            Assert.IsTrue(reset.WaitOne(1000));
             Assert.AreEqual(3, local.Count);
 
             //Remove
             collection.Remove(3);
-            Thread.Sleep(10);
+            Assert.IsTrue(reset.WaitOne(1000));
             Assert.AreEqual(2, local.Count);
 
             //GetItems
